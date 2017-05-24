dc330dde8fdbd4ef980a485c43507a41918cdab0
Peter Morlion <peter.morlion@gmail.com>
2017-05-24 10:00:14 +0200

    Add unit tests for RedStar.Amounts.StandardUnits

---
+++ RedStar.Amounts-netstandard/RedStar.Amounts.StandardUnits.Tests/RelativeUnitsTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace RedStar.Amounts.StandardUnits.Tests
+{
+    public class RelativeUnitsTests
+    {
+        [Fact]
+        public void WithPercentage_MultiplyingShouldDivide()
+        {
+            var percentage = new Amount(50, RelativeUnits.Percentage);
+            var amount = new Amount(40, LengthUnits.Meter);
+
+            var result = amount*percentage;
+
+            Assert.Equal(new Amount(20, LengthUnits.Meter), result);
+        }
+
+        [Fact]
+        public void WithAbsolute_MultiplyingShouldDivide()
+        {
+            var percentage = new Amount(0.5, RelativeUnits.Absolute);
+            var amount = new Amount(40, LengthUnits.Meter);
+
+            var result = amount * percentage;
+
+            Assert.Equal(new Amount(20, LengthUnits.Meter), result);
+        }
+    }
+}
