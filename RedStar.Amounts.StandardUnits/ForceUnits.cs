7337df011f4265bfb0c39ceb69c92b60d912d709
Peter Morlion <peter_morlion@tui.be>
2018-06-25 16:44:31 +0200

    Add dyne unit
    Related Work Items: #

--- RedStar.Amounts.StandardUnits/ForceUnits.cs
+++ RedStar.Amounts.StandardUnits/ForceUnits.cs
@@ -5,5 +5,7 @@ namespace RedStar.Amounts.StandardUnits
     {
         public static readonly Unit Newton = new Unit("newton", "N", LengthUnits.Meter * MassUnits.KiloGram * TimeUnits.Second.Power(-2));
         public static readonly Unit MicroNewton = new Unit("micronewton", "mN", 0.000001 * Newton);
+
+        public static readonly Unit Dyne = new Unit("dyne", "dyn", 0.00001 * Newton);
     }
 }
