b6436e46f50751ff08aad3a44e34f9d7722ff3b9
Peter Morlion <peter_morlion@tui.be>
2018-06-25 16:49:35 +0200

    Fix micro symbol in units
    Related Work Items: #

--- RedStar.Amounts.StandardUnits/ForceUnits.cs
+++ RedStar.Amounts.StandardUnits/ForceUnits.cs
@@ -4,7 +4,7 @@ namespace RedStar.Amounts.StandardUnits
     public static class ForceUnits
     {
         public static readonly Unit Newton = new Unit("newton", "N", LengthUnits.Meter * MassUnits.KiloGram * TimeUnits.Second.Power(-2));
-        public static readonly Unit MicroNewton = new Unit("micronewton", "mN", 0.000001 * Newton);
+        public static readonly Unit MicroNewton = new Unit("micronewton", "µN", 0.000001 * Newton);
 
         public static readonly Unit Dyne = new Unit("dyne", "dyn", 0.00001 * Newton);
     }
